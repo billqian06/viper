33f062068996ced213e8877ce901671b605cb917
Bill Qian <zq4@rice.edu>
2025-08-30 13:28:06 -0500

    update dependencies

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
 conda create -n vipergpt python=3.10
 conda activate vipergpt
-conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia
+conda install pytorch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install -r requirements.txt
